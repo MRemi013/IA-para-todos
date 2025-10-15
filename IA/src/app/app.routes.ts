e7885a1904083e4945726868572caebf60b4dd67
ariiialegria <arialegriahs1@gmail.com>
2025-10-15 18:53:59 -0300

    termine secciones problematicas e impacto-laboral

--- IA/src/app/app.routes.ts
+++ IA/src/app/app.routes.ts
@@ -1,21 +1,20 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { Confusiones } from './pages/confusiones/confusiones';
-import { Galeria } from './pages/galeria/galeria';
-import { ImpactoLaboral } from './pages/impacto-laboral/impacto-laboral';
-import { InfoGeneral } from './pages/info-general/info-general';
 import { Problematicas } from './pages/problematicas/problematicas';
-import { UsoCorrecto } from './pages/uso-correcto/uso-correcto';
+import { ImpactoLaboral } from './pages/impacto-laboral/impacto-laboral';
 import { VentajasDesventajas } from './pages/ventajas-desventajas/ventajas-desventajas';
+import { InfoGeneral } from './pages/info-general/info-general';
+import { Confusiones } from './pages/confusiones/confusiones';
+import { UsoCorrecto } from './pages/uso-correcto/uso-correcto';
+import { Galeria } from './pages/galeria/galeria';
 
 export const routes: Routes = [
-    {path:"",component:Home},
-    {path:"home",component:Home},
-    {path:"confusiones",component:Confusiones},
-    {path:"galeria",component:Galeria},
-    {path:"impacto laboral",component:ImpactoLaboral},
-    {path:"info general",component:InfoGeneral},
-    {path:"problematicas",component:Problematicas},
-    {path:"uso correcto",component:UsoCorrecto},
-    {path:"ventajas y desventajas",component:VentajasDesventajas}
+  { path: '', component: Home },
+  { path: 'problematicas', component: Problematicas },
+  { path: 'impacto-laboral', component: ImpactoLaboral },
+  { path: 'ventajas-desventajas', component: VentajasDesventajas },
+  { path: 'info-general', component: InfoGeneral },
+  { path: 'confusiones', component: Confusiones },
+  { path: 'uso-correcto', component: UsoCorrecto },
+  { path: 'galeria', component: Galeria }
 ];
