55145c53631b266c93b51b3b69fbb16cff626080
MRemi013 <luisemilianodiaz929@gmail.com>
2025-10-15 20:11:39 -0300

    PORFIN FUNCIONO

--- IA/src/app/app.ts
+++ IA/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { Home } from './pages/home/home';
-import { Confusiones } from './pages/confusiones/confusiones';
 import { Galeria } from './pages/galeria/galeria';
 import { ImpactoLaboral } from './pages/impacto-laboral/impacto-laboral';
 import { InfoGeneral } from './pages/info-general/info-general';
 import { Problematicas } from './pages/problematicas/problematicas';
 import { UsoCorrecto } from './pages/uso-correcto/uso-correcto';
-import { VentajasDesventajas } from './pages/ventajas-desventajas/ventajas-desventajas';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +14,11 @@ import { VentajasDesventajas } from './pages/ventajas-desventajas/ventajas-desve
     RouterOutlet,
     RouterLink,
     Home,
-    Confusiones,
     Galeria,
     ImpactoLaboral,
     InfoGeneral,
     Problematicas,
     UsoCorrecto,
-    VentajasDesventajas
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
